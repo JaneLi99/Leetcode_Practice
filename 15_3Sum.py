b6d139318139e83d836dabbd98659dffbb6fccf6
JaneLi99 <jane990423@gmail.com>
2022-06-14 09:35:41 -0400

    add one more solution

--- 15_3Sum.py
+++ 15_3Sum.py
@@ -1,18 +1,46 @@
 
 def threeSum(nums):
-    if len(nums) < 3:
-        return []
+    # if len(nums) < 3:
+    #     return []
+    #
+    # nums = sorted(nums)
+    # result = []
+    #
+    # for i in range(len(nums) - 1):
+    #     for j in range(i + 1, len(nums)):
+    #         two_sum = nums[i] + nums[j]
+    #         if -two_sum in nums[j + 1:]:
+    #             m = nums[j + 1:].index(-two_sum)
+    #             if [nums[i], nums[j], nums[j + 1:][m]] not in result:
+    #                 result.append([nums[i], nums[j], nums[j + 1:][m]])
+    # return result
 
-    nums = sorted(nums)
+    n = len(nums)
     result = []
+    nums.sort()
 
-    for i in range(len(nums) - 1):
-        for j in range(i + 1, len(nums)):
-            two_sum = nums[i] + nums[j]
-            if -two_sum in nums[j + 1:]:
-                m = nums[j + 1:].index(-two_sum)
-                if [nums[i], nums[j], nums[j + 1:][m]] not in result:
-                    result.append([nums[i], nums[j], nums[j + 1:][m]])
+    for i in range(n - 2):
+        if nums[i] + nums[i + 1] + nums[i + 2] > 0:
+            break
+        if nums[i] + nums[n - 2] + nums[n - 1] < 0:
+            continue
+        if 0 < i and nums[i] == nums[i - 1]:
+            continue
+        l, r = i + 1, n - 1
+        while l < r:
+            tmp = nums[i] + nums[l] + nums[r]
+            if tmp == 0:
+                result.append([nums[i], nums[l], nums[r]])
+                while l + 1 < r and nums[l] == nums[l + 1]:
+                    l += 1
+                l += 1
+                while l < r - 1 and nums[r] == nums[r - 1]:
+                    r -= 1
+                r -= 1
+            elif tmp < 0:
+                l += 1
+            else:
+                r -= 1
 
     print(result)
     return result
