aca7b927c80a091dccf7f694cb482484ea515220
JaneLi99 <jane990423@gmail.com>
2022-06-15 13:01:06 -0400

    June 15

---
+++ 8_String_to_Integer.py
@@ -0,0 +1,34 @@
+
+def myAtoi(s):
+    max_int = 2 ** 31 - 1
+    min_int = - 2 ** 31
+
+    res = 0
+    negative = 1
+    i = 0
+
+    while i < len(s) and s[i] == " ":
+        i = i + 1
+
+    if i < len(s) and s[i] == "-":
+        negative = -1
+        i = i + 1
+    elif i < len(s) and s[i] == "+":
+        i = i + 1
+
+    check_numbers = set("0123456789")
+    while i < len(s) and s[i] in check_numbers:
+        res = res * 10 + int(s[i])
+        i = i + 1
+
+    res = res * negative
+
+    if res < 0:
+        return max(res, min_int)
+    return min(res,max_int)
+
+if __name__ == '__main__':
+    print(myAtoi("42"))
+    print(myAtoi("    -42"))
+    print(myAtoi("4193 with words"))
+    print(myAtoi("eferf99-2"))
