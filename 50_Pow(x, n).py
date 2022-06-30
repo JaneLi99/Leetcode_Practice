307a0781961143308e1f8ccdf5a628c617280022
JaneLi99 <jane990423@gmail.com>
2022-06-30 10:39:49 -0400

    June 30

---
+++ 50_Pow(x, n).py
@@ -0,0 +1,8 @@
+def myPow(x, n):
+    res = pow(x, n)
+    return res
+
+if __name__ == '__main__':
+    print(myPow(x = 2.00000, n = 10))
+    print(myPow(x = 2.10000, n = 3))
+    print(myPow(x = 2.00000, n = -2))
