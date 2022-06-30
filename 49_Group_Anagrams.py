d6e84660f512ffa0bf07b69152d720efe71a4c4f
JaneLi99 <jane990423@gmail.com>
2022-06-30 10:37:50 -0400

    June 30

---
+++ 49_Group_Anagrams.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+def groupAnagrams(strs):
+    lookup = defaultdict(list)
+
+    for s in strs:
+        key = "".join(sorted(list(s)))
+        lookup[key].append(s)
+
+    return [l for l in lookup.values()]
+
+
+
+if __name__ == '__main__':
+    print(groupAnagrams(strs = ["eat","tea","tan"]))
+    print(groupAnagrams(strs = ["eat","tea","tan","ate","nat","bat"]))
+    print(groupAnagrams(strs = [""]))
+    print(groupAnagrams(strs = ["a"]))
