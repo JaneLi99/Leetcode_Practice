8f5b72149ea1494a4e6c8fb47f29aa0fce67ea3f
JaneLi99 <jane990423@gmail.com>
2022-08-08 18:28:34 -0400

    Aug 8

--- 32_Longest_Valid_Parentheses.py
+++ 32_Longest_Valid_Parentheses.py
@@ -13,10 +13,38 @@ def longestValidParentheses(s):
     return mx
 
 # Another Solution
+def longestValidParentheses_2(s):
+    l, r = 0, 0
+    mx = 0
+    for p in s:
+        if p == "(":
+            l += 1
+        else:
+            r += 1
+        if l == r:
+            mx = max(mx, r * 2)
+        elif r > l:
+            l, r = 0, 0
 
+    l, r = 0, 0
+    for p in reversed(s):
+        if p == ")":
+            r += 1
+        else:
+            l += 1
+        if l == r:
+            mx = max(mx, r * 2)
+        elif l > r:
+            l, r = 0, 0
+    return mx
 
 if __name__ == '__main__':
     print(longestValidParentheses(s = "(()"))
     print(longestValidParentheses(s = ""))
     print(longestValidParentheses(s = "(()())()))"))
     print(longestValidParentheses(s = ")()())()()("))
+
+    print(longestValidParentheses_2(s="(()"))
+    print(longestValidParentheses_2(s=""))
+    print(longestValidParentheses_2(s="(()())()))"))
+    print(longestValidParentheses_2(s=")()())()()("))
