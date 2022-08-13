ba5e379c13f7d4a07d32b97358f93a4a13350ac9
JaneLi99 <jane990423@gmail.com>
2022-08-13 11:58:34 -0400

    Aug 13

--- 76_Minimum_Window_Substring.py
+++ 76_Minimum_Window_Substring.py
@@ -1,42 +1,62 @@
+# Solution 1, O(n ^ 2)
 from collections import Counter
 
 def minWindow(s, t):
-    if len(t) > len(s) or t == "":
-        return ""
+    lookup = Counter(t)
+    mx = float("inf")
+    output = ""
+    S = len(s)
 
-    idx = []
-    for i in range(len(s)):
-        if s[i] in t:
-            idx.append(i)
-            t = t.replace(s[i], "")
-        if t == "":
-            break
+    def helper(a):
+        for k, v in lookup.items():
+            if k not in a or a[k] < v:
+                return False
+        return True
 
-    return idx
+    for end in range(S):
+        for start in range(end):
+            tmp = Counter(s[start : end + 1])
+            if helper(tmp):
+                if end - start < mx:
+                    mx = end - start
+                    output = s[start : end + 1]
+    return output
 
+# Another Solution, more efficient
+def minWindow_2(s, t):
+    lookup = Counter(t)
+    mx = float("inf")
+    output = ""
+    S = len(s)
+    start, end = 0, 0
+    count = len(lookup)
 
+    while end < S:
+        # End Pointer
+        while end < S and count != 0:
+            if s[end] in lookup:
+                lookup[s[end]] -= 1
+                if lookup[s[end]] == 0:
+                    count -= 1
+            end += 1
 
-class Solution:
-    def minWindow(self, s, t):
-        return
+        # Start Pointer
+        while start <= end and count == 0:
+            if end - start < mx:
+                mx = end - start
+                output = s[start : end]
+            if s[start] in lookup:
+                lookup[s[start]] += 1
+                if lookup[s[start]] > 0:
+                    count += 1
+            start += 1
+    return output
 
 if __name__ == '__main__':
     print(minWindow(s = "ADOBECODEBANC", t = "ABC"))
     print(minWindow(s = "a", t = "a"))
     print(minWindow(s = "a", t = "aa"))
 
-    s = "ADOBECODEBANC"
-    t = "ABC"
-
-    lookup = Counter(t)
-    print(lookup)
-    overall_list = []
-    for subt in t:
-        index_list = []
-        for i, ltr in enumerate(s):
-            if ltr == subt:
-                index_list.append(i)
-        overall_list.append(index_list)
-    print(overall_list)
-
-    diff = []
+    print(minWindow_2(s = "ADOBECODEBANC", t = "ABC"))
+    print(minWindow_2(s="a", t="a"))
+    print(minWindow_2(s="a", t="aa"))
