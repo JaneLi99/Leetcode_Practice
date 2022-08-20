28969aa1c02f5ecde5b5eb2a75970e93ce01d10f
JaneLi99 <jane990423@gmail.com>
2022-08-19 21:23:04 -0400

    Aug 19

---
+++ 344_Reverse_String.py
@@ -0,0 +1,9 @@
+def reverseString(s):
+    """
+    Do not return anything, modify s in-place instead.
+    """
+    s.reverse()
+
+if __name__ == '__main__':
+    print(reverseString(s = ["h","e","l","l","o"]))
+    print(reverseString(s = ["H","a","n","n","a","h"]))
