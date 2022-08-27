dcf297b8736ca941d00eb5fbe752ed82cbdeb0c5
JaneLi99 <jane990423@gmail.com>
2022-08-27 07:07:39 -0400

    Aug 27

---
+++ 415_Add_Strings.py
@@ -0,0 +1,8 @@
+def addStrings(num1, num2):
+    sum = int(num1) + int(num2)
+    return str(sum)
+
+if __name__ == '__main__':
+    print(addStrings(num1 = "11", num2 = "123"))
+    print(addStrings(num1 = "456", num2 = "77"))
+    print(addStrings(num1 = "0", num2 = "0"))
