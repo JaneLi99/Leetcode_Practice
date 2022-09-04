1fc0c70c2937d501f9525b4cdfc6a112840012bf
JaneLi99 <jane990423@gmail.com>
2022-09-04 12:04:43 -0400

    Sept 4

--- Remove_Strings_Contains_Substrings.py
+++ Remove_Strings_Contains_Substrings.py
@@ -1,4 +1,9 @@
-# Record the algorithm problems I encountered during developing tools at Ford
+"""
+Record the algorithm problems I encountered during developing tools at Ford
+The below script is called by searching part, and it is used to delete the extra file names.
+It can check and simplify the multiple search files and make the program more efficient.
+E.g. input = ["apple", "appleeee", "applejuice", "orange", "oranges", "banana"], output = [“apple", "orange", "banana”].
+"""
 
 def remove_dup(list):
     # Check if search strings have the substrings
@@ -8,46 +13,29 @@ def remove_dup(list):
         idx_1, idx_2 = 0, len(list) - 1
         idx_2_times = 0
 
-        """
-        eg. search_file_split = ["slog", "slogggger", "slog", "slogg", "fffnvv", "fnv", "fdp"]
-        The idx_1 search from the beginning of the search_file_split and idx_2 search from the end of the list
-        """
         while idx_1 < idx_2:
             if list[idx_1] in list[idx_2]:
-                """
-                Check if the string idx_1 is the substring of string idx_2
-                If yes, it will remove the string idx_2
-                """
                 list.remove(list[idx_2])
                 idx_2 = len(list) - 1
                 idx_2_times = 0
+
             elif list[idx_2] in list[idx_1]:
-                """
-                Check if the string idx_2 is the substring of string idx_1
-                If yes, it will remove the string idx_1
-                """
                 list.remove(list[idx_1])
                 idx_2 = len(list) - 1
                 idx_2_times = 0
+
             else:
-                """
-                If there's no matched substrings, idx_2 will move to the next one
-                """
                 idx_2_times = idx_2_times + 1
                 idx_2 = idx_2 - 1
 
-            """
-            If finished checking the first string and there's no matched in the following elements
-            for example: ["slog", "fnv", "fdppp", "fdp"], since there's no "slog" in ["fnv", "fdppp", "fdp"],
-            then idx_1 moves to the next one to check "fnv" based on idx_2_times, 
-            if still not found, it moves to "fdppp".
-            """
             if idx_2_times == len(list[idx_1:]) - 1:
                 idx_1 = idx_1 + 1
                 idx_2 = len(list) - 1
                 idx_2_times = 0
+
     return list
 
+# Test some cases
 if __name__ == '__main__':
-    print(remove_dup(list = ["fdp","slogger", "ffnv", "slog", "fnv", "ffdpp", "ffffnvv", "sloggg"]))
+    print(remove_dup(list = ["apple", "appleee", "orange", "pears", "pear"]))
     print(remove_dup(list = ["aaabcd", "bcd"]))
